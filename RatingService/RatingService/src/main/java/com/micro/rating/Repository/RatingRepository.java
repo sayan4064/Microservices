54335f9de722ca010ee3a58d5f062416e0c558d7
sayan <metyasayan3@gmail.com>
2026-09-12 22:22:18 +0530

    Initial commit

--- RatingService/RatingService/src/main/java/com/micro/rating/Repository/RatingRepository.java
+++ RatingService/RatingService/src/main/java/com/micro/rating/Repository/RatingRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface RatingRepository extends JpaRepository<Rating,String> {
-    List<Rating> getRatingsByUserId(String userId);
-    List<Rating> getRatingsByHotelId(String hotelId);
+public interface RatingRepository extends JpaRepository<Rating, UUID> {
+    List<Rating> getRatingsByUserId(UUID userId);
+    List<Rating> getRatingsByHotelId(UUID hotelId);
 }
